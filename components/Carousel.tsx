52727e3b672ace27fa683de4179bdc04fec76734
mascanho <mascanho@sapo.pt>
2023-04-13 23:28:06 +0100

    Update Carousel.tsx

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -5,60 +5,64 @@ import Image from "next/image";
 
 function CarouselHero() {
   return (
-    <Carousel
-      withIndicators
-      height={200}
-      slideSize="33.333333%"
-      slideGap="md"
-      loop
-      align="start"
-      breakpoints={[
-        { maxWidth: "md", slideSize: "50%" },
-        { maxWidth: "sm", slideSize: "100%", slideGap: 0 },
-      ]}
-    >
-      <Carousel.Slide>
-        <div className="w-full h-40 flex flex-col rounded-t-md overflow-hidden relative">
-          <Image
-            src="https://th.bing.com/th/id/OIG.XUrUyoz7q_uPku3p7E.0?pid=ImgGn"
-            fill
-            alt="image"
-            className="block"
-          />
-          <span className="absolute left-0 top-4 pr-2 py-1 rounded-r-full text-xs text-black bg-white  ">
-            {/* 📍 {data.city} */}
-          </span>
-        </div>
-      </Carousel.Slide>
-      <Carousel.Slide>
-        <div className="w-full h-40 flex flex-col rounded-t-md overflow-hidden relative">
-          <Image
-            src="https://th.bing.com/th/id/OIG.XUrUyoz7q_uPku3p7E.0?pid=ImgGn"
-            fill
-            alt="image"
-            className="block"
-          />
-          <span className="absolute left-0 top-4 pr-2 py-1 rounded-r-full text-xs text-black bg-white  ">
-            {/* 📍 {data.city} */}
-          </span>
-        </div>
-      </Carousel.Slide>
-      <Carousel.Slide>
-        <div className="w-full h-40 flex flex-col rounded-t-md overflow-hidden relative">
-          <Image
-            src="https://th.bing.com/th/id/OIG.XUrUyoz7q_uPku3p7E.0?pid=ImgGn"
-            fill
-            alt="image"
-            className="block"
-          />
-          <span className="absolute left-0 top-4 pr-2 py-1 rounded-r-full text-xs text-black bg-white  ">
-            {/* 📍 {data.city} */}
-          </span>
-        </div>
-      </Carousel.Slide>
+    <>
+      <section>
+        <Carousel
+          withIndicators
+          height={300}
+          slideSize="33.333333%"
+          slideGap="md"
+          loop
+          align="start"
+          breakpoints={[
+            { maxWidth: "md", slideSize: "50%" },
+            { maxWidth: "sm", slideSize: "100%", slideGap: 0 },
+          ]}
+        >
+          <Carousel.Slide>
+            <div className="w-full h-full flex flex-col rounded-md overflow-hidden relative">
+              <Image
+                src="https://th.bing.com/th/id/OIG.XUrUyoz7q_uPku3p7E.0?pid=ImgGn"
+                fill
+                alt="image"
+                className="block"
+              />
+              <span className="absolute left-0 top-4 pr-2 py-1 rounded-r-full text-xs text-black bg-white  ">
+                {/* 📍 {data.city} */}
+              </span>
+            </div>
+          </Carousel.Slide>
+          <Carousel.Slide>
+            <div className="w-full h-full flex flex-col rounded-md overflow-hidden relative">
+              <Image
+                src="https://th.bing.com/th/id/OIG.XUrUyoz7q_uPku3p7E.0?pid=ImgGn"
+                fill
+                alt="image"
+                className="block"
+              />
+              <span className="absolute left-0 top-4 pr-2 py-1 rounded-r-full text-xs text-black bg-white  ">
+                {/* 📍 {data.city} */}
+              </span>
+            </div>
+          </Carousel.Slide>
+          <Carousel.Slide>
+            <div className="w-full h-full flex flex-col rounded-md overflow-hidden relative">
+              <Image
+                src="https://th.bing.com/th/id/OIG.XUrUyoz7q_uPku3p7E.0?pid=ImgGn"
+                fill
+                alt="image"
+                className="block"
+              />
+              <span className="absolute left-0 top-4 pr-2 py-1 rounded-r-full text-xs text-black bg-white  ">
+                {/* 📍 {data.city} */}
+              </span>
+            </div>
+          </Carousel.Slide>
 
-      {/* ...other slides */}
-    </Carousel>
+          {/* ...other slides */}
+        </Carousel>
+      </section>
+    </>
   );
 }
 
