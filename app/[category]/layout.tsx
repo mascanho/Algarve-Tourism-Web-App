2ba18f9c9e78c17bcbe571f5062daac0c21d4eff
mascanho <mascanho@sapo.pt>
2023-04-29 12:02:09 +0100

    Update layout.tsx

--- app/[category]/layout.tsx
+++ app/[category]/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
       <Hero />
       <section className="max-w-7xl mx-auto flex pt-10 sm:pt-20 justify-start transition-all ease-in delay-75 sticky top-11 mb-72  h-[100%]">
         <SidePanel />
-        <div className="mb-20 flex-1 h-full">{children}</div>
+        <div className="flex-1 h-full mb-20">{children}</div>
       </section>
     </>
   );
