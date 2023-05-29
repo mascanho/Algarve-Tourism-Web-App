7329194321537e6960a7f7bf376f3fb723a900e8
mascanho <mascanho@sapo.pt>
2023-05-29 15:54:32 +0100

    Update DrawerContent.tsx

--- components/modals/DrawerContent.tsx
+++ components/modals/DrawerContent.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const DrawerContent = () => {
   return [1, 2, 3, 4, 5].map((item) => (
-    <div key={item} className="my-2">
+    <div key={item} className="my-4">
       <Link href="/">
         <div className="border p-2 cursor-pointer hover:bg-sky group  hover:text-white hover:border-white rounded-md relative flex flex-col justify-start text-left transition-all ease-in delay-75">
           <span className="absolute right-2 text-xs text-gray-500 transition-all ease-in delay-75 group-hover:text-red-400">
@@ -14,7 +14,7 @@ const DrawerContent = () => {
               <img
                 src="https://www.albufeiraportugaltourism.com/images/praia-da-gale-algarve.jpg"
                 alt="dwd"
-                className="w-full h-32 rounded-md object-fill"
+                className="w-full h-28 rounded-md object-fill"
               />
               <div className="w-full mx-auto flex flex-col mt-2">
                 <h3 className="text-sm">Praia da Gale 😲🙊</h3>
