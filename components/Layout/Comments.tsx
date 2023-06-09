ed05131b069b6c0ee0a6370edb837deeb89863f3
mascanho <mascanho@sapo.pt>
2023-06-09 22:41:51 +0100

    Update Comments.tsx

--- components/Layout/Comments.tsx
+++ components/Layout/Comments.tsx
@@ -1,7 +1,8 @@
 // import { getServerSession } from "next-auth";
 import Comment from "../Comment";
 import CommentForm from "../CommentForm";
-async function Comments() {
+
+function Comments() {
   return (
     <section className="flex justify-start mt-5">
       <div className="flex items-center flex-col justify-start text-left w-full space-y-3">
