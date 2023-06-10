2d707a5eaf649e4178cb531a04ccd4326d2c342b
mascanho <mascanho@sapo.pt>
2023-06-10 22:55:51 +0100

    Update getComments.ts

--- app/libs/getComments.ts
+++ app/libs/getComments.ts
@@ -1,6 +1,6 @@
 import prisma from "../libs/prismadb";
 
-export default async function getComments(slug: string): Promise<any> {
+export default async function getComments(): Promise<any> {
   const comments: any = await prisma.comments
     .findMany
     // {
