cd1e2e81f0ee7a6e4189f7abf905e6aeb98db43e
mascanho <mascanho@sapo.pt>
2023-06-17 19:33:07 +0100

    pnpm lock removed

--- app/algarve/page.tsx
+++ app/algarve/page.tsx
@@ -30,6 +30,7 @@ function page() {
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-10 sm:mt-20 sm:gap-x-8 overflow-hidden mx-auto w-full sm:gap-y-20 gap-y-8 max-w-7xl justify-stretch place-items-center  place-content-between">
         {cityArr.map((city: any) => (
           <CardCity
+            key={city.id}
             city={city}
             name={city.name}
             image={city.image}
