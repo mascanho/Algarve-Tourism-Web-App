feabee0d242c5bbadfe14b0ec4ca8d3197fa41d5
mascanho <mascanho@sapo.pt>
2023-06-23 23:24:27 +0100

    Blog card update

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,7 +5,13 @@ function page() {
   return (
     <section className="h-screen sm:max-w-7xl sm:w-full mx-auto">
       <div className="relative flex justify-center items-center w-full overflow-hidden ">
-        <div className="sm:border-r sm:w-8/12 block w-full h-screen mt-10 mx-auto sm:ml-0">
+        <div className="sm:border-r sm:w-8/12 block w-full h-screen pt-10 mx-auto sm:ml-0">
+          <div className="mx-auto sm:w-11/12 my-6 ">
+            <input
+              className=" mx-auto rounded-lg bg-transparent border py-1 pl-2 w-8/12"
+              placeholder="Search..."
+            />
+          </div>
           <h2 className="sm:w-11/12 mx-auto w-11/12 border-b pb-5 font-bold text-black">
             Articles
           </h2>
@@ -17,13 +23,18 @@ function page() {
           </div>
         </div>
         <div className="w-1/3 sm:pl-6  h-screen hidden sm:inline-block">
-          <div>
-            <div>
-              <h3>This is a title</h3>
-              <p>
+          <div className="mt-10">
+            <div className="bg-gray-200 p-4 py-6 rounded-lg space-y-2">
+              <h3 className="text-black font-bold text-lg">This is a title</h3>
+              <p className="text-sm">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 Numquam, odio.
               </p>
+              <div className="mt-2">
+                <button className="bg-sky rounded-md px-4 py-1 mt-2 text-white">
+                  Read More
+                </button>
+              </div>
             </div>
           </div>
         </div>
