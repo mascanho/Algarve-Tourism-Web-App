af908f6208fea2d5af8bcf79a59263d0744d3f99
mascanho <mascanho@sapo.pt>
2023-06-23 23:07:24 +0100

    blog card created

--- app/blog/BlogCard.tsx
+++ app/blog/BlogCard.tsx
@@ -3,33 +3,43 @@ import React from "react";
 export const BlogCard = () => {
   return (
     <>
-      <section className="flex border-b pb-4">
+      <section className="flex sm:w-11/12 w-11/12 mx-auto">
         <div className="avatar">
           <div className="w-12 rounded-full h-12">
             <img src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/05/Ichigo-Kurosaki.jpg" />
           </div>
         </div>
-        <div className="block">
-          <div className="flex items-center">
-            <h4>MArco Guerreiro</h4>
-            <span>Apr 20, 2022</span>
+        <div className="block w-full h-full">
+          <div className="flex items-center pl-2 space-y-1">
+            <h4 className="font-bold flex-items text-black">MArco Guerreiro</h4>
+            <div className="flex items-center">
+              <span className="pb-1 text-sm ml-2">Apr 20, 2022</span>
+            </div>
           </div>
-          <div className="block">
+          <div className="block pl-2 text-sm">
             <span>Lorem ipsum dolor sit amet.</span>
-            <div>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel,
-              itaque!
-            </div>
-            <div className="flex w-ful">
-              {[1, 2, 3].map((item) => (
-                <span>{item}</span>
-              ))}
-            </div>
           </div>
         </div>
-        <picture className="w-40 h-full">
+        <picture className="w-40 h-full sm:inline-block hidden rounded-md overflow-hidden">
           <img src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/05/Ichigo-Kurosaki.jpg" />
         </picture>
+      </section>
+      <section className="sm:w-11/12 mx-auto">
+        <div className="pb-2 sm:w-2/3 w-11/12 mx-auto sm:mx-0 mt-3 sm:mt-1">
+          <p className="text-sm">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
+            dolorem natus sunt voluptatum consequuntur aut consequatur placeat.
+            Alias, vitae saepe.
+          </p>
+        </div>
+        <div className="flex sm:w-full w-11/12 mx-auto mt-1 pb-8">
+          {[1, 2, 3].map((item) => (
+            <span className="border mr-2 px-4 rounded-2xl sm:text-xs bg-gray-100">
+              {item}
+            </span>
+          ))}
+        </div>
+        <hr className="block sm:w-full mx-auto pb-8" />
       </section>
     </>
   );
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,15 +3,20 @@ import { BlogCard } from "./BlogCard";
 
 function page() {
   return (
-    <section className="h-screen max-w-7xl mx-auto">
-      <div className="relative flex justify-center items-center w-full overflow-hidden  bg-cover bg-blend-multiply h-screen">
-        <div className="border-r w-2/3 block h-screen">
-          <h2 className="w-11/12 border-b font-bold text-black">Articles</h2>
-          <div>
+    <section className="h-screen sm:max-w-7xl sm:w-full mx-auto">
+      <div className="relative flex justify-center items-center w-full overflow-hidden ">
+        <div className="sm:border-r sm:w-8/12 block w-full h-screen mt-10 mx-auto sm:ml-0">
+          <h2 className="sm:w-11/12 mx-auto w-11/12 border-b pb-5 font-bold text-black">
+            Articles
+          </h2>
+          <div className="mx-auto mt-5">
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
             <BlogCard />
           </div>
         </div>
-        <div className="w-1/3 block h-screen">
+        <div className="w-1/3 sm:pl-6  h-screen hidden sm:inline-block">
           <div>
             <div>
               <h3>This is a title</h3>
