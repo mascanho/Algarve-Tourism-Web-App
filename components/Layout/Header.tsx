83ab2366af082b302f0fde7410e834efcd4349a9
mascanho <mascanho@sapo.pt>
2023-09-17 11:48:33 +0100

    navbar updates

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -20,6 +20,7 @@ import useAddToFavourites from "@/app/hooks/useAddToFavourites";
 import Link from "next/link";
 import { cityArr } from "@/Data/Cities";
 import { BsSearch } from "react-icons/bs";
+import { AiOutlineHome } from "react-icons/ai";
 
 interface UserProps {
   currentUser: {
@@ -331,24 +332,11 @@ const Header = ({ currentUser }: UserProps) => {
 
         {bottomNav ? (
           <div className="btm-nav sm:hidden">
-            <button className="active border-teal-400">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                />
-              </svg>
+            <button>
+              <AiOutlineHome className="mt-2" />
             </button>
             <button className="active:border-gray-700">
-              <BsSearch />
+              <BsSearch className="mt-2" />
             </button>
             <button>
               <svg
