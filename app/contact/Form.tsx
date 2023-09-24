710d5d99e373ac8670ae92af5f1a2b26acbadcc3
mascanho <mascanho@sapo.pt>
2023-09-24 14:22:59 +0100

    Form spreee added

--- app/contact/Form.tsx
+++ app/contact/Form.tsx
@@ -1,29 +1,49 @@
 "use client";
 
+import { useForm, ValidationError } from "@formspree/react";
+import Link from "next/link";
+
 export default function Form() {
+  const [state, handleSubmit] = useForm("xeqbrndo");
+  if (state.succeeded) {
+    return (
+      <>
+        <p className="text-center">We got your message!</p>
+        <Link href="/">
+          <button className="border bg-sky text-white w-full py-2 mt-8">
+            Back to Home Page
+          </button>
+        </Link>
+      </>
+    );
+  }
+
   return (
-    <form className=" m-auto flex flex-col space-y-6 w-full">
+    <form
+      onSubmit={handleSubmit}
+      className=" m-auto flex flex-col space-y-6 w-full"
+      action="https://formspree.io/f/xeqbrndo"
+      method="POST"
+    >
       <input
-        type="text"
-        placeholder="Type here"
+        placeholder="Email"
         className="input input-bordered input-info w-full bg-transparent"
+        id="email"
+        type="email"
+        name="email"
       />{" "}
-      <input
-        type="text"
-        placeholder="Type here"
-        className="input input-bordered input-info w-full bg-transparent "
-      />{" "}
-      <input
-        type="text"
-        placeholder="Type here"
-        className="input input-bordered input-info w-full bg-transparent "
-      />{" "}
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
       <textarea
         className="textarea textarea-info bg-transparent"
-        placeholder="Bio"
-      ></textarea>{" "}
+        placeholder="Your Message..."
+      />
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
       <div className="pt-2 w-full">
-        <button className="border bg-sky text-white w-full py-2 mt-8">
+        <button
+          type="submit"
+          className="border bg-sky text-white w-full py-2 mt-8"
+          disabled={state.submitting}
+        >
           Send
         </button>
       </div>
