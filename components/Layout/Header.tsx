e52afd6cc638c447bc1a322586984d245f2b39a1
mascanho <mascanho@sapo.pt>
2023-09-24 13:25:16 +0100

    Modal update

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -332,54 +332,54 @@ const Header = ({ currentUser }: any) => {
 
         {/* Bottom Navigation */}
 
-        {bottomNav ? (
-          <div className="fixed bottom-0 sm:hidden">
-            <button>
-              <AiOutlineHome className="mt-2" />
-            </button>
-            <button className="active:border-gray-700">
-              <Link href="/beaches">
-                <BsSearch className="mt-2" />
-              </Link>
-            </button>
-            <button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
-              </svg>
-            </button>
-            <button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
-              </svg>
-            </button>
-          </div>
-        ) : (
-          ""
-        )}
+        {/* {bottomNav ? ( */}
+        {/*   <div className="fixed bottom-0 sm:hidden"> */}
+        {/*     <button> */}
+        {/*       <AiOutlineHome className="mt-2" /> */}
+        {/*     </button> */}
+        {/*     <button className="active:border-gray-700"> */}
+        {/*       <Link href="/beaches"> */}
+        {/*         <BsSearch className="mt-2" /> */}
+        {/*       </Link> */}
+        {/*     </button> */}
+        {/*     <button> */}
+        {/*       <svg */}
+        {/*         xmlns="http://www.w3.org/2000/svg" */}
+        {/*         className="h-5 w-5" */}
+        {/*         fill="none" */}
+        {/*         viewBox="0 0 24 24" */}
+        {/*         stroke="currentColor" */}
+        {/*       > */}
+        {/*         <path */}
+        {/*           strokeLinecap="round" */}
+        {/*           strokeLinejoin="round" */}
+        {/*           strokeWidth="2" */}
+        {/*           d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" */}
+        {/*         /> */}
+        {/*       </svg> */}
+        {/*     </button> */}
+        {/*     <button> */}
+        {/*       <svg */}
+        {/*         xmlns="http://www.w3.org/2000/svg" */}
+        {/*         className="h-5 w-5" */}
+        {/*         fill="none" */}
+        {/*         viewBox="0 0 24 24" */}
+        {/*         stroke="currentColor" */}
+        {/*       > */}
+        {/*         <path */}
+        {/*           strokeLinecap="round" */}
+        {/*           strokeLinejoin="round" */}
+        {/*           strokeWidth="2" */}
+        {/*           d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" */}
+        {/*         /> */}
+        {/*       </svg> */}
+        {/*     </button> */}
+        {/*   </div> */}
+        {/* ) : ( */}
+        {/*   "" */}
+        {/* )} */}
       </nav>
-      <Modal opened={opened} onClose={close} title="Authentication" centered>
+      <Modal opened={opened} onClose={close} title="Favourite Places" centered>
         {/* Modal content */}
         <DrawerContent />
       </Modal>
