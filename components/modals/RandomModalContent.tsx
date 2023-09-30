c82a2b06b1a55451e77a9845dd362e6e18b91a56
mascanho <mascanho@sapo.pt>
2023-09-30 23:40:15 +0100

    Update RandomModalContent.tsx

--- components/modals/RandomModalContent.tsx
+++ components/modals/RandomModalContent.tsx
@@ -8,19 +8,27 @@ function RandomModalContent() {
 
   return (
     <>
-      <div className="mx-auto w-full flex">
-        <SegmentedControl
-          color="blue"
-          data={["React", "Angular", "Vue", "marco"]}
-          fullWidth
-          onClick={(e: any) => console.log(e.target.innerText)}
-        />
-      </div>
-      <Chip.Group multiple>
-        <Chip value="1">Multiple chips</Chip>
-        <Chip value="2">Can be selected</Chip>
-        <Chip value="3">At a time</Chip>
-      </Chip.Group>{" "}
+      {page === 1 ? (
+        <>
+          <div className="mx-auto w-full flex">
+            <SegmentedControl
+              color="blue"
+              data={["React", "Angular", "Vue", "marco"]}
+              fullWidth
+              onClick={(e: any) => console.log(e.target.innerText)}
+            />
+          </div>
+          <Chip.Group multiple>
+            <Chip value="1">Multiple chips</Chip>
+            <Chip value="2">Can be selected</Chip>
+            <Chip value="3">At a time</Chip>
+          </Chip.Group>{" "}
+        </>
+      ) : (
+        <h1>Page 2</h1>
+      )}
+      <button onClick={() => setPage(2)}>next</button>
+      <button onClick={() => setPage(1)}>prev</button>
     </>
   );
 }
