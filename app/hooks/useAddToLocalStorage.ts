a2044db8fd782dbc765b6c00a8e63f60d032693f
mascanho <mascanho@sapo.pt>
2023-12-28 14:39:25 +0000

    Delete useAddToLocalStorage.ts

--- app/hooks/useAddToLocalStorage.ts
+++
@@ -1,6 +0,0 @@
-export const useAddToLocalStorage = (key: string, value: any) => {
-  const addToLocalStorage = (key: string, value: any) => {
-    localStorage.setItem(key, JSON.stringify(value));
-    console.log("Key: " key, "Value: " value);
-  };
-};
