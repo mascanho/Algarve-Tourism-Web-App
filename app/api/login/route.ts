faf66ca342734b94ff7b222286760fa291d0043a
mascanho <mascanho@sapo.pt>
2024-01-04 20:43:25 +0000

    Update route.ts

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
+import { NextApiRequest } from "next";
 import bcrypt from "bcrypt";
 
 import prisma from "@/app/libs/prismadb";
 
 export async function GET(req: NextApiRequest) {
-  const email = req.body?.email;
+  const email = req?.body?.email as string;
   const user = await prisma.user.findUnique({
     where: { email: email },
     select: { id: true, name: true, email: true },
