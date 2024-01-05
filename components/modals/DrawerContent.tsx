6482b6521d0ed94c5f35f7607767076c935e1c6a
mascanho <mascanho@sapo.pt>
2024-01-05 21:06:46 +0000

    Update DrawerContent.tsx

--- components/modals/DrawerContent.tsx
+++ components/modals/DrawerContent.tsx
@@ -12,41 +12,43 @@ const DrawerContent = ({ close, title }: any) => {
 
   return (
     <>
-      {favourites.map((item) => (
-        <div key={item} className="my-4">
-          <AnimatePresence>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              className="shadow p-2 cursor-pointer hover:bg-sky group  hover:text-white hover:border-white rounded-md relative flex flex-col justify-start text-left transition-all ease-in delay-75"
-            >
-              <span
-                onClick={() => removeFavourite(item.id)}
-                className="absolute right-2 bottom-3 text-xs text-gray-500 transition-all ease-in delay-75 group-hover:text-black"
+      {favourites.map((item, index) => (
+        <div key={index} className="my-4">
+          <Link href={`/${item.type}/${item.id}`} onClick={close}>
+            <AnimatePresence>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="shadow p-2 cursor-pointer hover:bg-sky group  hover:text-white hover:border-white rounded-md relative flex flex-col justify-start text-left transition-all ease-in delay-75"
               >
-                <FaRegTrashAlt className="group-hover:text-white" />
-              </span>
-              <div className="text-left flex  space-x-3">
-                <div className="flex items-center">
-                  <img
-                    src={item?.image}
-                    alt="dwd"
-                    className="w-24 h-16 rounded-md object-fill m-auto"
-                  />
-                </div>
-                <div className="w-full mx-auto flex flex-col mt-2 space-y-[2px]">
-                  <h3 className="text-sm font-semibold">{item.title}</h3>
-                  <div className="flex items-start text-left text-xs">
-                    <StarRating rating={item.rating} />
+                <span
+                  onClick={() => removeFavourite(item.id)}
+                  className="absolute right-2 bottom-3 text-xs text-gray-500 transition-all ease-in delay-75 group-hover:text-black"
+                >
+                  <FaRegTrashAlt className="group-hover:text-white" />
+                </span>
+                <div className="text-left flex  space-x-3">
+                  <div className="flex items-center">
+                    <img
+                      src={item?.image}
+                      alt="dwd"
+                      className="w-24 h-16 rounded-md object-fill m-auto"
+                    />
                   </div>
-                  <p className="text-sm text-gray-400 group-hover:text-gray-600">
-                    {item.city}
-                  </p>
+                  <div className="w-full mx-auto flex flex-col mt-2 space-y-[2px]">
+                    <h3 className="text-sm font-semibold">{item.title}</h3>
+                    <div className="flex items-start text-left text-xs">
+                      <StarRating rating={item.rating} />
+                    </div>
+                    <p className="text-sm text-gray-400 group-hover:text-gray-600">
+                      {item.city}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </motion.div>
-          </AnimatePresence>
+              </motion.div>
+            </AnimatePresence>
+          </Link>
         </div>
       ))}
 
