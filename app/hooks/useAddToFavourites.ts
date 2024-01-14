df9b47cd4e403104387967a935d19e9f0b448896
mascanho <mascanho@sapo.pt>
2024-01-14 03:14:31 +0000

    Update useAddToFavourites.ts

--- app/hooks/useAddToFavourites.ts
+++ app/hooks/useAddToFavourites.ts
@@ -27,7 +27,9 @@ const useAddToFavourites = create<FavouriteProps>((set) => ({
       }
 
       const updatedFavourites = [data, ...state.favourites];
-      localStorage?.setItem("favourites", JSON.stringify(updatedFavourites));
+      if (typeof window !== "undefined") {
+        localStorage?.setItem("favourites", JSON.stringify(updatedFavourites));
+      }
 
       return {
         favourites: updatedFavourites,
@@ -39,8 +41,9 @@ const useAddToFavourites = create<FavouriteProps>((set) => ({
       const updatedFavourites = state.favourites.filter(
         (item) => item.id !== id
       );
-      localStorage?.setItem("favourites", JSON.stringify(updatedFavourites));
-
+      if (typeof window !== "undefined") {
+        localStorage?.setItem("favourites", JSON.stringify(updatedFavourites));
+      }
       return {
         favourites: updatedFavourites,
       };
