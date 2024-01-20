167629da25b614aaf863457f184b9bd042aa5299
mascanho <mascanho@sapo.pt>
2024-01-20 15:10:40 +0000

    no images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import Search from "./Search";
 
 const IMAGES_DESKTOP = [
-  "https://lp-cms-production.imgix.net/2022-05/GettyRF_1013112160.jpg?auto=format&w=1440&h=810&fit=crop&q=75",
+  "/images/bg1.jpeg",
   "https://fullsuitcase.com/wp-content/uploads/2023/05/Lagos-the-nicest-town-in-Algarve-Portugal.jpg.webp",
   "https://escales.ponant.com/wp-content/uploads/2023/07/iStock-621690956-1.jpg",
   "https://turismo.eurodicas.com.br/wp-content/uploads/2019/07/praias-do-algarve-1.jpg",
@@ -20,18 +20,18 @@ const Hero = ({ categories }: any) => {
       />
       <div
         className={` hidden sm:block animate-fade w-screen  bg-cover  bg-blend-multiply bg-black/70  h-screen`}
-        style={{
-          backgroundImage: `url(${
-            IMAGES_DESKTOP[Math.floor(Math.random() * IMAGES_DESKTOP.length)]
-          })`,
-        }}
+        // style={{
+        //   backgroundImage: `url(${
+        //     IMAGES_DESKTOP[Math.floor(Math.random() * IMAGES_DESKTOP.length)]
+        //   })`,
+        // }}
       />
       <section className="mx-auto w-full sm:h-screen space-y-2 flex flex-col justify-center text-center m-auto absolute top-64  sm:top-2 md:top-0 ">
         <h1 className="mx-auto text-5xl font-bold text-center text-white sm:text-8xl ">
           Algarve&apos;s Travel Guide
         </h1>
         <div className=" rounded-lg w-7/12 sm:w-full mx-auto p-2 hidden sm:block">
-          <h2 className="hidden sm:block  mx-auto rounded-sm w-9/12 sm:w-10/12 px-4  text-white/70 ">
+          <h2 className="hidden sm:block  mx-auto rounded-sm w-9/12 sm:w-10/12 lg:w-2/4 px-4  text-white/70 ">
             Discover the breathtaking beauty of the Algarve! The ultimate guide
             to the region&apos;s top tourism attractions and less known hidden
             gems
