c38ab1c621c41eabd9290586459e193d19d6516c
mascanho <mascanho@sapo.pt>
2024-01-21 20:04:43 +0000

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -80,6 +80,9 @@ export default async function Home(props: any) {
 
   return (
     <>
+      <div className="relative">
+        <Hero categories={categories} />
+      </div>
       <section className="pt-14 sm:pt-20 pb-6 space-y-4 text-cente-white overflow-hidden">
         <div className="mb-28">
           <Features />
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = ({ categories }: any) => {
 
   return (
     <>
-      <div className="h-screen w-screen">
+      <div>
         <img
           src="/images/bg-mobile.webp"
           className="w-screen h-screen object-cover filter brightness-50"
