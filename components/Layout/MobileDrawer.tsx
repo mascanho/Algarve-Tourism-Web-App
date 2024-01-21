79c8ba3eb9498bfbcd1f099580eda96cf48963e7
mascanho <mascanho@sapo.pt>
2024-01-21 19:25:47 +0000

    Update MobileDrawer.tsx

--- components/Layout/MobileDrawer.tsx
+++ components/Layout/MobileDrawer.tsx
@@ -21,7 +21,6 @@ const MobileDrawer = ({ currentUser }: any) => {
         </button>
       </Drawer.Trigger>
       <Drawer.Portal>
-        <Drawer.Overlay className="fixed inset-0 bg-black/40" />
         <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] mt-24 fixed bottom-0 left-0 right-0">
           <div className="p-4 bg-white rounded-t-[10px] flex-1">
             <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
@@ -29,23 +28,6 @@ const MobileDrawer = ({ currentUser }: any) => {
               <Drawer.Title className="font-medium mb-4">
                 Unstyled drawer for React.
               </Drawer.Title>
-
-              {currentUser ? (
-                <p>Logout</p>
-              ) : (
-                <div>
-                  <p className="text-zinc-600 mb-2" onClick={loginModal.onOpen}>
-                    Sign In{" "}
-                  </p>
-
-                  <p
-                    className="text-zinc-600 mb-8"
-                    onClick={registeredModal.onOpen}
-                  >
-                    Sign In
-                  </p>
-                </div>
-              )}
             </div>
           </div>
           <div className="p-4 bg-zinc-100 border-t border-zinc-200 mt-auto">
