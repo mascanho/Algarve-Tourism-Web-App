f45ad65d71ac5969556281e1a29e4e4b2e497b30
mascanho <mascanho@sapo.pt>
2024-01-21 18:56:11 +0000

    Update Footer.tsx

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 import useAddToFavourites from "@/app/hooks/useAddToFavourites";
 import Link from "next/link";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { BsArrowUp } from "react-icons/bs";
 import { FaArrowUp, FaHeart } from "react-icons/fa";
 import { HiArrowUp } from "react-icons/hi2";
@@ -12,12 +12,13 @@ function Footer() {
   const [showToTop, setShowToTop] = useState(false);
   const { favourites } = useAddToFavourites();
   const [favouritesLength, setFavouritesLength] = useState(0);
-  const [fakeOnlineUsers, setFakeOnlineUsers] = useState(0);
 
   const [isScrollingUp, setIsScrollingUp] = useState(true);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
 
-  console.log(favourites.length, "from the footer");
+  const [fakeOnlineUsers, setFakeOnlineUsers] = useState(() =>
+    Math.floor(Math.random() * 1000),
+  );
 
   useEffect(() => {
     const handleScroll = () => {
@@ -46,17 +47,6 @@ function Footer() {
     };
   }, []);
 
-  // UPDATE THE ONLINE USERS SO IT DOES NOT RERENDER EVERY TIME THE USER SCROLLS
-  useEffect(() => {
-    const handleOnlineUsers = () => {
-      setFakeOnlineUsers(Math.floor(Math.random() * 100));
-    };
-    window.addEventListener("online", handleOnlineUsers);
-    return () => {
-      window.removeEventListener("online", handleOnlineUsers);
-    };
-  }, []);
-
   // SHOW THE ELEMENT CONDITIONALY DEPENDING ON SCROLL. IF SCROLING UP SHOW THE ELEMENT, IF SCROLLING DOWN HIDE THE ELEMENT
   const handleYposition = useCallback(() => {
     const currentScrollPos = window.pageYOffset;
@@ -77,8 +67,21 @@ function Footer() {
 
   useEffect(() => {
     const favouritesFromLocalStorage = localStorage.getItem("favourites");
-    setFavouritesLength(JSON.parse(favouritesFromLocalStorage).length);
-  }, [favourites]);
+    const favouritesArray = JSON.parse(favouritesFromLocalStorage) || [];
+    setFavouritesLength(favouritesArray.length);
+  }, [favourites.length]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setFakeOnlineUsers((prevOnlineUsers: any) => {
+        // You can update the online users based on the previous value if needed
+        // For now, we're generating a random number in this example
+        return Math.floor(Math.random() * 1000);
+      });
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <>
@@ -209,39 +212,6 @@ function Footer() {
                 <span>users online</span>
                 <span className="w-2 h-2 rounded-full bg-green-500/60" />
               </div>
-              {/* <a> */}
-              {/*   <svg */}
-              {/*     xmlns="http://www.w3.org/2000/svg" */}
-              {/*     width="24" */}
-              {/*     height="24" */}
-              {/*     viewBox="0 0 24 24" */}
-              {/*     className="fill-current" */}
-              {/*   > */}
-              {/*     <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path> */}
-              {/*   </svg> */}
-              {/* </a> */}
-              {/* <a> */}
-              {/*   <svg */}
-              {/*     xmlns="http://www.w3.org/2000/svg" */}
-              {/*     width="24" */}
-              {/*     height="24" */}
-              {/*     viewBox="0 0 24 24" */}
-              {/*     className="fill-current" */}
-              {/*   > */}
-              {/*     <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path> */}
-              {/*   </svg> */}
-              {/* </a> */}
-              {/* <a> */}
-              {/*   <svg */}
-              {/*     xmlns="http://www.w3.org/2000/svg" */}
-              {/*     width="24" */}
-              {/*     height="24" */}
-              {/*     viewBox="0 0 24 24" */}
-              {/*     className="fill-current" */}
-              {/*   > */}
-              {/*     <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path> */}
-              {/*   </svg> */}
-              {/* </a> */}
             </div>
           </div>
         </footer>
