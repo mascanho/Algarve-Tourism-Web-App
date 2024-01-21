6a619c4865ec1433bc45daf96c499d20018902ae
mascanho <mascanho@sapo.pt>
2024-01-21 17:02:19 +0000

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,7 +15,7 @@ const Hero = ({ categories }: any) => {
   return (
     <>
       <div
-        className={`sm:hidden  animate-fade overflow-x-hidden  bg-blend-multiply bg-black/60 bg-[url("/images/bg-mobile.webp")]   h-screen w-screen`}
+        className={`sm:hidden  animate-fade overflow-x-hidden  bg-blend-multiply bg-black/60 bg-[url("/images/bg-mobile.webp")] bg-cover   h-screen w-screen`}
       />
       <div
         className={` overflow-x-hidden hidden sm:block animate-fade bg-fixed bg-cover  bg-blend-multiply bg-black/70  h-dvh`}
