857a6ca95061e12b9f14f15cf2e5249a7237c845
mascanho <mascanho@sapo.pt>
2024-01-26 16:33:27 +0000

    Update Search.tsx

--- components/Search.tsx
+++ components/Search.tsx
@@ -97,11 +97,11 @@ function Search({ allTypes, placeholderText, categories }: any) {
             id="search"
             name="search"
           />
-          <IoIosSearch className="absolute top-6 left-3" />
+          <IoIosSearch className="absolute top-6 sm:left-3 left-8" />
           <div />
           <button
             type="submit"
-            className="bg-sky absolute active:bg-gray-400 right-1 text-xs sm:right-2 top-4 sm:top-[14px] rounded-full sm:px-6 px-5 font-semibold py-2 sm:py-2 text-white sm:text-sm"
+            className="bg-sky absolute active:bg-gray-400 right-4 text-xs sm:right-2 top-4 sm:top-[14px] rounded-full sm:px-6 px-5 font-semibold py-2 sm:py-2 text-white sm:text-sm"
           >
             {/* <BsSearch className="text-xl" /> */}
             Search
