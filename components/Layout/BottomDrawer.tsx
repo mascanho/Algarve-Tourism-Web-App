3369495c447aa34fe1290915aa85c171e4839c56
mascanho <mascanho@sapo.pt>
2024-01-26 18:10:01 +0000

    updated stable UI

--- components/Layout/BottomDrawer.tsx
+++ components/Layout/BottomDrawer.tsx
@@ -8,10 +8,14 @@ import { AiOutlineBell } from "react-icons/ai";
 import { BiChevronRight } from "react-icons/bi";
 import { useRouter } from "next/navigation";
 import { IoIosClose } from "react-icons/io";
+import useAddToFavourites from "@/app/hooks/useAddToFavourites";
+import { useEffect, useState } from "react";
 
 function BottomDrawer({ favouritesLength }: any) {
   const [opened, { open, close }] = useDisclosure(true);
   const router = useRouter();
+  const [favourites, setFavourites] = useState<any[]>([]);
+  const { removeFavourite } = useAddToFavourites();
 
   const favouritesFromLocalStorage =
     typeof localStorage !== "undefined"
@@ -19,6 +23,38 @@ function BottomDrawer({ favouritesLength }: any) {
       : null;
 
   const favouritesArray = JSON.parse(favouritesFromLocalStorage) || [];
+
+  const removeFavouriteGlobal = (id: number) => {
+    // Remove the favorite from the local state
+    const updatedFavourites = favourites.filter((fav: any) => fav.id !== id);
+    removeFavourite(id);
+    setFavourites(updatedFavourites);
+    console.log(id, "from the drawer");
+
+    // Update local storage
+    localStorage.setItem("favourites", JSON.stringify(updatedFavourites));
+  };
+
+  useEffect(() => {
+    document.title = "Algarve Wonders - Your Favourites";
+    let link: HTMLLinkElement | null =
+      document.querySelector("link[rel~='icon']");
+    if (!link) {
+      link = document.createElement("link");
+      link.rel = "icon";
+      document.head.appendChild(link);
+    }
+    link.href = "/images/icon.png";
+
+    // Initialization logic outside the store creation
+    if (typeof window !== "undefined") {
+      const favourites = localStorage?.getItem("favourites");
+      if (favourites) {
+        useAddToFavourites.setState({ favourites: JSON.parse(favourites) });
+        setFavourites(JSON.parse(favourites));
+      }
+    }
+  }, [removeFavourite]);
 
   return (
     <>
@@ -57,15 +93,20 @@ function BottomDrawer({ favouritesLength }: any) {
                 <section
                   className="border text-xs relative flex mb-2  h-full rounded-md items-center"
                   key={item.id}
-                  onClick={() => {
-                    router.push(`/${item?.type}/${item?.slug}`);
-                    close();
-                  }}
                 >
-                  <div className="flex flex-wrap  bg-gray-200 h-10 px-1 ">
+                  <div
+                    onClick={() => removeFavouriteGlobal(item?.id)}
+                    className="flex flex-wrap  bg-gray-200 h-10 px-1 "
+                  >
                     <IoIosClose className="h-full" />
                   </div>
-                  <div className="flex flex-col  mt-1 flex-wrap w-full ml-2 justify-around ">
+                  <div
+                    onClick={() => {
+                      router.push(`/${item?.type}/${item?.slug}`);
+                      close();
+                    }}
+                    className="flex flex-col  mt-1 flex-wrap w-full ml-2 justify-around "
+                  >
                     <span className="font-semibold text-black">
                       {" "}
                       {item?.title}
