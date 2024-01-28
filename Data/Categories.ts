1c57299794a7a5e671a6b05341c2b165023b8c76
mascanho <mascanho@sapo.pt>
2024-01-28 17:49:43 +0000

    Update Categories.ts

--- Data/Categories.ts
+++ Data/Categories.ts
@@ -1,11 +1,38 @@
 export const catArr = [
   {
+    id: 4,
+    name: "Adventure",
+    icon: "",
+    route: "/adventure",
+    image:
+      "https://blog-cdn.checkyeti.com/wp-content/uploads/2020/07/Skydiving_in_the_Algarve.jpg",
+    type: "category",
+  },
+  {
     id: 2,
     name: "Beaches",
     icon: "",
     route: "/beaches",
     image:
       "https://gretastravels.com/wp-content/uploads/2021/09/DSC_0911.jpg.webp",
+    type: "category",
+  },
+  {
+    id: 9,
+    name: "Business",
+    icon: "",
+    route: "/business",
+    image:
+      "https://altrincham.todaynews.co.uk/content/images/size/w1304/wp-content/uploads/sites/7/2022/10/realalgarve.webp",
+    type: "category",
+  },
+  {
+    id: 8,
+    name: "Events",
+    icon: "",
+    route: "/events",
+    image:
+      "https://www.walkalgarve.com/wp-content/uploads/2016/10/SandsSculpture-1.jpg",
     type: "category",
   },
   {
@@ -18,12 +45,12 @@ export const catArr = [
     type: "category",
   },
   {
-    id: 4,
-    name: "Adventure",
+    id: 7,
+    name: "Restaurants",
     icon: "",
-    route: "/adventure",
+    route: "/restaurants",
     image:
-      "https://blog-cdn.checkyeti.com/wp-content/uploads/2020/07/Skydiving_in_the_Algarve.jpg",
+      "https://images.myguide-cdn.com/md/md/common/large/60a78291ea22e-716694.jpg",
     type: "category",
   },
   {
@@ -42,33 +69,6 @@ export const catArr = [
     route: "/stays",
     image:
       "https://cf.bstatic.com/static/img/theme-index/bg_farmhouses/141149a16a0d641093ab4e9c11e5a365d71e6e8b.jpg",
-    type: "category",
-  },
-  {
-    id: 7,
-    name: "Restaurants",
-    icon: "",
-    route: "/restaurants",
-    image:
-      "https://images.myguide-cdn.com/md/md/common/large/60a78291ea22e-716694.jpg",
-    type: "category",
-  },
-  {
-    id: 8,
-    name: "Events",
-    icon: "",
-    route: "/events",
-    image:
-      "https://www.walkalgarve.com/wp-content/uploads/2016/10/SandsSculpture-1.jpg",
-    type: "category",
-  },
-  {
-    id: 9,
-    name: "Business",
-    icon: "",
-    route: "/business",
-    image:
-      "https://altrincham.todaynews.co.uk/content/images/size/w1304/wp-content/uploads/sites/7/2022/10/realalgarve.webp",
     type: "category",
   },
 ];
