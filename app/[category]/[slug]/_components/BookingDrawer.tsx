8ea7686cc7b2816574ac36ea8925c83111054ccb
mascanho <mascanho@sapo.pt>
2024-01-31 16:30:15 +0000

    Update BookingDrawer.tsx

--- app/[category]/[slug]/_components/BookingDrawer.tsx
+++ app/[category]/[slug]/_components/BookingDrawer.tsx
@@ -15,10 +15,17 @@ const BookingDrawer = ({ url, title }: string) => {
         position="bottom"
         size={"100%"}
         radius="md"
+        className="overflow-hidden"
       >
         {/* CONTENT  */}
 
-        <iframe src={url} width="100%" height="800px" title={title} />
+        <iframe
+          src={url}
+          width="100%"
+          height="800px"
+          title={title}
+          className="h-[100vh] scrollbar-hide"
+        />
       </Drawer>
       <span onClick={open} className="text-sm underline text-gray-700">
         {title}
