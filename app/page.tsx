53f65d386cdde7985175e6d36104e26405262123
mascanho <mascanho@sapo.pt>
2024-02-04 13:40:33 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default async function Home(props: any) {
 
   return (
     <section className="w-full">
-      <Hero categories={categories} />
+      {/* <Hero categories={categories} /> */}
       <Features />
       <div className="space-y-4 my-10 hidden mt-12 sm:mt-10 mx-auto text-center sm:flex sm:flex-col sm:justify-center w-11/12 max-w-7xl ">
         <h3 className="text-4xl font-bold text-black sm:text-5xl mt-20 hidden sm:flex sm:text-center sm:justify-center sm:mx-auto">
