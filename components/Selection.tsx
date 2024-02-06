4f99991c1b5ad94e2611968b8fe54c666d1a4f49
mascanho <mascanho@sapo.pt>
2024-02-06 22:55:48 +0000

    UI finalised

--- components/Selection.tsx
+++ components/Selection.tsx
@@ -18,15 +18,35 @@ const Selection = () => {
 
   const handleClick = (cat) => {
     setActiveCategory(cat);
-    // Get the DOM element of the category to scroll into view
-    const categoryElement = document.getElementById(cat.route);
-    // If the category element exists, scroll it into view
-    if (categoryElement) {
-      categoryElement.scrollIntoView({ behavior: "smooth" });
+
+    // Determine the index of the selected category within catArr
+    const selectedIndex = catArr.findIndex(
+      (category) => category.name === cat.name,
+    );
+
+    // If the carouselRef is available and the index is valid
+    if (carouselRef.current && selectedIndex !== -1) {
+      // Calculate the scroll position based on the index and the width of each slide
+      const slideWidth = carouselRef.current.offsetWidth;
+      const scrollLeft = selectedIndex * slideWidth;
+
+      // Scroll the carousel to the calculated position
+      carouselRef.current.scrollTo({
+        left: scrollLeft,
+        behavior: "smooth",
+      });
+    }
+
+    // Get the DOM element of the category link to scroll into view
+    const categoryLink = document.getElementById(`category-link-${cat.route}`);
+
+    // If the category link element exists, scroll it into view
+    if (categoryLink) {
+      categoryLink.scrollIntoView({ behavior: "smooth", block: "nearest" });
     }
   };
 
-  const categoryIcons: any = {
+  const categoryIcons = {
     Beaches: <GiBeachBucket />,
     Restaurants: <FaUtensils />,
     Events: <MdEvent />,
@@ -38,10 +58,10 @@ const Selection = () => {
   };
 
   return (
-    <section className="w-full inline text-center   sm:hidden overflow-x-clip fixed top-8  z-20 bg-white  h-18 pt-3 ">
+    <section className="w-full inline text-center sm:hidden overflow-x-clip fixed top-8 z-20 bg-white h-18 pt-3">
       {pathname === "/" ? null : (
         <>
-          <section className="overflow-hidden w-full shadow-2xl border-b py-1">
+          <section className="overflow-hidden w-full shadow-xl border-b py-1">
             <Carousel
               ref={carouselRef} // Assign the ref to the Carousel component
               dragFree
@@ -53,31 +73,30 @@ const Selection = () => {
               {catArr.map((cat) => (
                 <div
                   className={`
-                   px-2 text-xs mr-2  my-1 text-right flex justify-end h-auto w-[95px] `}
-                  key={cat?.name}
+                                        px-2 text-xs mr-2 my-1 text-right flex justify-end h-auto w-[95px]`}
+                  key={cat.name}
                   onClick={() => handleClick(cat)}
                 >
-                  <Link href={cat?.route}>
-                    <div className="flex flex-col items-center rounded-xl justify-center ">
+                  <Link href={cat.route} id={`category-link-${cat.route}`}>
+                    <div className="flex flex-col items-center rounded-xl justify-center">
                       <span
                         className={`text-2xl text-gray-400 ${
-                          pathname?.includes(cat.route)
+                          pathname.includes(cat.route)
                             ? "text-key"
                             : "text-gray-400"
                         }`}
                       >
-                        {" "}
                         {categoryIcons[cat.name]}
                       </span>
                       <span
                         className={`ml-1 ${
-                          pathname?.includes(cat.route)
+                          pathname.includes(cat.route)
                             ? "text-key"
                             : "text-gray-400"
                         }`}
                         id={cat.route} // Assign id to the span element
                       >
-                        {cat?.name}
+                        {cat.name}
                       </span>
                     </div>
                   </Link>
