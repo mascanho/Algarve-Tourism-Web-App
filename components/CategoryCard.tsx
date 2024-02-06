c7f500dfe5079b36af5ba2a3fdff9c81f56b2a7b
mascanho <mascanho@sapo.pt>
2024-02-06 21:00:41 +0000

    mobile selection

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -55,10 +55,8 @@ const CategoryCard: React.FC<CategoryCardProps> = ({
 
   return (
     <>
-      <div>
-        <div className="sm:hidden mx-auto flex justify-center w-11/12 sm:mb-8">
-          <Selection text="Filter your adventure" />
-        </div>
+      <div className="overflow-hidden w-full">
+        <Selection text="Filter your adventure" />
         <section className="max-w-7xl transition-all ease-in delay-75 mx-auto w-11/12 sm:w-full  sm:px-6  grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  md:ml-auto gap-y-8 md:gap-x-6  mb-20">
           {category.map((cat: any) => (
             <section
--- components/Selection.tsx
+++ components/Selection.tsx
@@ -4,49 +4,100 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { usePathname } from "next/navigation";
 import { Box, ScrollArea, Select } from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
+import {
+  MdBusiness,
+  MdBusinessCenter,
+  MdEvent,
+  MdSportsHandball,
+} from "react-icons/md";
+import { PiMountainsFill } from "react-icons/pi";
+import {
+  FaArrowLeft,
+  FaArrowRight,
+  FaHiking,
+  FaUtensils,
+} from "react-icons/fa";
+import { GiBeachBucket, GiWoodCabin } from "react-icons/gi";
+import { useState } from "react";
+import { rem } from "@mantine/core";
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
 
-const Selection = ({ text }: any) => {
+const categoryIcons: any = {
+  Beaches: <GiBeachBucket />,
+  Restaurants: <FaUtensils />,
+  Events: <MdEvent />,
+  Adventure: <PiMountainsFill />,
+  Business: <MdBusinessCenter />,
+  Hiking: <FaHiking />,
+  Sports: <MdSportsHandball />,
+  Stays: <GiWoodCabin />,
+};
+
+const Selection = () => {
   const router = useRouter();
   const pathname = usePathname();
-
+  const [activeCategory, setActiveCategory] = useState(null);
   const goToPage = (e: any) => {
     const route = e.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
     router.push(`${route}`);
   };
 
-  // matching the placeholder of the dropdown select with the path and title of category
-  let placeholder = "";
-
-  const matching = catArr.find((cat) => cat.route === pathname);
-
-  if (matching) {
-    placeholder = matching.name;
-  }
-
-  // when the pathname is "/"
-  if (pathname === "/") {
-    // Add your code here for when the pathname is "/"
-    // ...
-    placeholder = "Select your category";
-  }
+  const handleClick = (cat: any) => {
+    setActiveCategory(cat);
+  };
 
   return (
-    <>
+    <section className="w-full inline  sm:hidden overflow-x-clip fixed bottom-0 z-10 bg-darkwhite border-t px-1 h-18 pt-1 ">
       {pathname === "/" ? null : (
-        <section className="flex max-w-7xl w-full sm:hidden mx-auto px-2 py-2 overflow-hidden">
-          <ScrollArea w={400} h={40} scrollbars="y">
-            <Box w={500} className="">
+        <>
+          <span className="absolute -top-[13px] bg-darkwhite px-6 py-1 m-auto rounded-t-xl left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            {pathname}
+          </span>
+          <section className="overflow-hidden w-full">
+            <Carousel
+              nextControlIcon={
+                <BiChevronRight style={{ width: rem(16), height: rem(16) }} />
+              }
+              previousControlIcon={
+                <BiChevronLeft style={{ width: rem(16), height: rem(16) }} />
+              }
+              align={"center"}
+              className="w-full overflow-hidden mx-auto flex flex-wrap"
+            >
               {catArr.map((cat: any) => (
-                <span className="mr-2 text-key text-sm" key={cat?.id}>
-                  {cat?.name}
-                </span>
+                <div
+                  className={`py-1  ${
+                    activeCategory === cat
+                      ? "bg-key rounded-xl w-20 text-white"
+                      : ""
+                  } px-2 text-xs mr-2  my-1 text-right flex justify-end h-auto w-[95px] `}
+                  key={cat}
+                  onClick={() => handleClick(cat)}
+                >
+                  <Link href={`${cat.route}`}>
+                    <div className="flex flex-col items-center rounded-xl justify-center ">
+                      <span
+                        className={`text-2xl text-key ${activeCategory === cat ? "text-white" : "text-key"}`}
+                      >
+                        {" "}
+                        {categoryIcons[cat.name]}
+                      </span>
+                      <span
+                        className={`ml-1 ${activeCategory === cat ? "text-white" : "text-key"}`}
+                      >
+                        {cat?.name}
+                      </span>
+                    </div>
+                  </Link>
+                </div>
               ))}
-            </Box>
-          </ScrollArea>
-        </section>
+            </Carousel>
+          </section>
+        </>
       )}
-    </>
+    </section>
   );
 };
 
