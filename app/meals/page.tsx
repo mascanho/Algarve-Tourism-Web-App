5ebb2070926ece28555fc3db47e976f5d83a627f
mascanho <mascanho@sapo.pt>
2024-02-10 16:03:08 +0000

    search functionality finished

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -6,17 +6,25 @@ import SearchMeal from "./_components/SearchMeal";
 
 // get the data from Prisma
 
-async function MealsPage() {
+async function MealsPage(params: any) {
+  const city = params?.searchParams?.city;
+
   let allMeals = await prisma?.dailymeal?.findMany({});
 
-  console.log(allMeals);
+  const search = await prisma?.dailymeal?.findMany({
+    where: {
+      city: city,
+    },
+  });
+
+  console.log(city, search);
 
   return (
     <div className="h-screen pt-20 w-full">
       <h1 className="text-black mb-10 text-xl font-semibold text-center">
         Today's daily meals
       </h1>
-      <SegmentTab meals={allMeals} />
+      <SegmentTab meals={allMeals} search={search} />
     </div>
   );
 }
