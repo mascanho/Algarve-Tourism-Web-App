d9c2a5c3e877e28186e90dd3dbae621303058a22
mascanho <mascanho@sapo.pt>
2024-02-10 11:43:13 +0000

    Update page.tsx

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -17,11 +17,14 @@ async function MealsPage() {
         return (
           <div
             key={meal.id}
-            className="w-11/12 mx-auto border p-2 rounded-xl bg-white"
+            className="w-11/12 mx-auto border p-2 rounded-xl bg-white mb-2"
           >
             <h3 className="font-bold text-black">{meal?.business}</h3>
             <h3 className="text-black">{meal?.meal}</h3>
-            <h3 className="text-black">{meal?.price}</h3>
+            <h3 className="text-black">
+              €{""}
+              {meal?.price}
+            </h3>
             <h3 className="text-black">{meal?.city}</h3>
           </div>
         );
