10e3343fbbedffb59ce8f6b6bcaf0b6303a7fcbb
mascanho <mascanho@sapo.pt>
2024-02-11 18:20:14 +0000

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,49 +7,62 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: "https://www.algarvewonders.com/blog/",
+      url: "https://www.algarvewonders.com/blog",
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://www.algarvewonders.com/beaches/",
+      url: "https://www.algarvewonders.com/beaches",
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://www.algarvewonders.com/events/",
+      url: "https://www.algarvewonders.com/events",
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://www.algarvewonders.com/restaurants/",
+      url: "https://www.algarvewonders.com/restaurants",
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://www.algarvewonders.com/sports/",
+      url: "https://www.algarvewonders.com/sports",
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://www.algarvewonders.com/business/",
+      url: "https://www.algarvewonders.com/business",
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://www.algarvewonders.com/stays/",
+      url: "https://www.algarvewonders.com/stays",
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://www.algarvewonders.com/adventure/",
+      url: "https://www.algarvewonders.com/adventure",
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://www.algarvewonders.com/hiking/",
+      url: "https://www.algarvewonders.com/hiking",
       changeFrequency: "weekly",
       priority: 1,
     },
+<<<<<<< Updated upstream
+=======
+    {
+      url: "https://www.algarvewonders.com/meals",
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://www.algarvewonders.com/upload",
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+>>>>>>> Stashed changes
   ];
 }
