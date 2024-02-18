1cabe4e77a3f863bf0e4dfa2a6f8c8acaa2a269d
mascanho <mascanho@sapo.pt>
2024-02-18 16:08:14 +0000

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -164,7 +164,7 @@ const Card = ({
               <section className="flex justify-between bg-key shadow-md rounded-b-lg w-full py-2 items-center">
                 <div className="flex items-center  w-full px-2 align-middl space-x-3">
                   {/* <BsQrCodeScan className="hover:scale-110 cursor-pointer" /> */}
-                  <a aria-label={`View on map`} href={mapShare} target="_blank">
+                  <a href={mapShare} aria-label={`View on map`} target="_blank">
                     <BiMap className="hover:scale-110 cursor-pointer" />
                   </a>
                   <BsShareFill
