b7a46a8e7f5b6e4caeca7d5b43c8a0b806f80fdb
mascanho <mascanho@sapo.pt>
2024-05-19 16:37:52 +0100

    Update page.tsx

--- app/planner/page.tsx
+++ app/planner/page.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { cookies } from "next/headers";
 
-// retrieve the cookies from the server
-const getCookie = (name: string) => {
-  const cookieStore = cookies();
-  return cookieStore.get(name)?.value;
-};
-
-const data = getCookie("builderData");
-const cookieData = JSON.parse(data || "{}");
-
 console.log(cookieData.days, "From the Page itself");
 
 const JourneyPage = () => {
+  // retrieve the cookies from the server
+  const getCookie = (name: string) => {
+    const cookieStore = cookies();
+    return cookieStore.get(name)?.value;
+  };
+
+  const data = getCookie("builderData");
+  const cookieData = JSON.parse(data || "{}");
+
   return <div>JourneyPage</div>;
 };
 
