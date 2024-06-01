73df2ea3fdb7673db170ddb14ec8f295f991e72c
mascanho <mascanho@sapo.pt>
2024-06-01 14:37:23 +0100

    Update Cities.ts

--- Data/Cities.ts
+++ Data/Cities.ts
@@ -105,4 +105,13 @@ export const cityArr = [
     image: "https://www.iberian-escapes.com/images/Praia-de-tavira.jpg",
     type: "city",
   },
+  {
+    id: 13,
+    name: "Vila Real de Santo António",
+    icon: "",
+    route: "vila-real-de-santo-antonio",
+    image:
+      "https://www.travel-in-portugal.com/sites/default/files/vila-real-santo-antonio.jpg",
+    type: "city",
+  },
 ];
