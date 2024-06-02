740aafdeabb6d8ec71c23e160ae6820d8c60b507
mascanho <mascanho@sapo.pt>
2024-06-02 11:21:33 +0100

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,261 +1,85 @@
+import { createClient } from "contentful";
 import { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+// Function to fetch items from Contentful
+export async function fetchItems() {
+  const sitemapArr = [
     {
-      url: "https://www.algarvewonders.com/search",
+      url: "https://www.algarvewonders.com/",
       changeFrequency: "weekly",
       priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/algarve",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/blog",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/adventure",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/stays/luna-solaqua",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/beaches/fuzeta-beach",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/beaches/monte-gordo-beach",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/beaches/manta-rota-beach",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/beaches/carvoeiro-beach",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/beaches/vale-covo-beach",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/beaches/amoreira-beach",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/submit",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/beaches",
-      changeFrequency: "weekly",
-      priority: 1,
+      lastmod: new Date().toISOString(),
     },
     {
       url: "https://www.algarvewonders.com/about",
       changeFrequency: "weekly",
       priority: 1,
+      lastmod: new Date().toISOString(),
     },
     {
-      url: "https://www.algarvewonders.com/changelog",
+      url: "https://www.algarvewonders.com/blog",
       changeFrequency: "weekly",
       priority: 1,
+      lastmod: new Date().toISOString(),
     },
     {
-      url: "https://www.algarvewonders.com/legal",
+      url: "https://www.algarvewonders.com/algarve",
       changeFrequency: "weekly",
       priority: 1,
+      lastmod: new Date().toISOString(),
     },
     {
-      url: "https://www.algarvewonders.com/partner",
+      url: "https://www.algarvewonders.com/meals",
       changeFrequency: "weekly",
       priority: 1,
+      lastmod: new Date().toISOString(),
+    },
+    {
+      url: "https://www.algarvewonders.com/search",
+      changeFrequency: "weekly",
+      priority: 1,
+      lastmod: new Date().toISOString(),
     },
     {
       url: "https://www.algarvewonders.com/contact",
       changeFrequency: "weekly",
       priority: 1,
+      lastmod: new Date().toISOString(),
     },
-    {
-      url: "https://www.algarvewonders.com/advertise",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/algarve/albufeira",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/algarve/alvor",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/algarve/carvoeiro",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/algarve/faro",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/algarve/lagos",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/algarve/loule",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/algarve/olhao",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/algarve/portimao",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/algarve/quarteira",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/algarve/sagres",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/algarve/silves",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/algarve/tavira",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/blog/things-to-do-in-albufeira",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/blog/discover-the-timeless-allure-of-algarve-tourism-as-we-journey-through-sunlit",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/sports/surf-expert-school",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/sports/kitesurf-algarve",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/beaches/camilo-beach",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/beaches/marinha-beach",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/beaches/praia-de-santa-eulalia",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/business",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/culture",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/events",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/hiking",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/restaurants",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/sports",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/stays",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/adventure/canhoto-wine-tasting-farm",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/adventure/lagos-zoo",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/adventure/marina-de-albufeira",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/adventure/paragliding-and-paratrike-tandem-flights",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/adventure/zebra-safaris",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/adventure/zoomarine-algarve",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-  ];
+  ]; // Initialize an empty array to store sitemap items
+
+  try {
+    const client = createClient({
+      space: process.env.CONTENTFUL_SPACE_ID!,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+    });
+
+    const res = await client.getEntries({
+      content_type: ["beaches", "events", "restaurants", "adventure"],
+    });
+
+    // Process each item and push to sitemapArr
+    //
+
+    console.log(res.items);
+
+    res.items.forEach((item: any) => {
+      sitemapArr.push({
+        url: `https://www.algarvewonders.com/${item?.fields?.type}/${item.fields.slug}`,
+        changeFrequency: "weekly",
+        priority: 1,
+        lastmod: new Date().toISOString(),
+      });
+    });
+
+    console.log(sitemapArr);
+    return sitemapArr; // Return sitemap array
+  } catch (error) {
+    console.error("Error fetching Contentful entries:", error);
+    return []; // Return empty array in case of error
+  }
+}
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return fetchItems();
 }
