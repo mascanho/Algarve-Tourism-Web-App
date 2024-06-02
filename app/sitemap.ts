3da78c1f096980ddfee6c59073532a46128086b1
mascanho <mascanho@sapo.pt>
2024-06-02 11:21:56 +0100

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,81 +1,85 @@
+import { createClient } from "contentful";
 import { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+// Function to fetch items from Contentful
+export async function fetchItems() {
+  const sitemapArr = [
     {
-      url: "https://www.algarvewonders.com",
+      url: "https://www.algarvewonders.com/",
       changeFrequency: "weekly",
       priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/blog",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/beaches",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/events",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/restaurants",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/sports",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/business",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/stays",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/adventure",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/hiking",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/meals",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/upload",
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://www.algarvewonders.com/algarve",
-      changeFrequency: "weekly",
-      priority: 1,
+      lastmod: new Date().toISOString(),
     },
     {
       url: "https://www.algarvewonders.com/about",
       changeFrequency: "weekly",
       priority: 1,
+      lastmod: new Date().toISOString(),
+    },
+    {
+      url: "https://www.algarvewonders.com/blog",
+      changeFrequency: "weekly",
+      priority: 1,
+      lastmod: new Date().toISOString(),
+    },
+    {
+      url: "https://www.algarvewonders.com/algarve",
+      changeFrequency: "weekly",
+      priority: 1,
+      lastmod: new Date().toISOString(),
+    },
+    {
+      url: "https://www.algarvewonders.com/meals",
+      changeFrequency: "weekly",
+      priority: 1,
+      lastmod: new Date().toISOString(),
+    },
+    {
+      url: "https://www.algarvewonders.com/search",
+      changeFrequency: "weekly",
+      priority: 1,
+      lastmod: new Date().toISOString(),
     },
     {
       url: "https://www.algarvewonders.com/contact",
       changeFrequency: "weekly",
       priority: 1,
+      lastmod: new Date().toISOString(),
     },
-  ];
+  ]; // Initialize an empty array to store sitemap items
+
+  try {
+    const client = createClient({
+      space: process.env.CONTENTFUL_SPACE_ID!,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+    });
+
+    const res = await client.getEntries({
+      content_type: ["beaches", "events", "restaurants", "adventure"],
+    });
+
+    // Process each item and push to sitemapArr
+    //
+
+    console.log(res.items);
+
+    res.items.forEach((item: any) => {
+      sitemapArr.push({
+        url: `https://www.algarvewonders.com/${item?.fields?.type}/${item.fields.slug}`,
+        changeFrequency: "weekly",
+        priority: 1,
+        lastmod: new Date().toISOString(),
+      });
+    });
+
+    console.log(sitemapArr);
+    return sitemapArr; // Return sitemap array
+  } catch (error) {
+    console.error("Error fetching Contentful entries:", error);
+    return []; // Return empty array in case of error
+  }
+}
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return fetchItems();
 }
